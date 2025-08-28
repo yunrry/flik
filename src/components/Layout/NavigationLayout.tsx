fd59c9dc1e87104ddf727b2f789153148e95b16b
yunrry <yunryy329@gmail.com>
2025-08-28 22:51:49 +0900

    fix: RegionPage scroll bug

--- src/components/Layout/NavigationLayout.tsx
+++ src/components/Layout/NavigationLayout.tsx
@@ -16,7 +16,8 @@ const NavigationLayout: React.FC<NavigationLayoutProps> = ({
   const shouldShowNavigation = showNavigation && isNavigationPage;
 
   return (
-    <div className={`h-screen bg-gray-50 flex flex-col ${disableScroll ? 'overflow-hidden' : ''}`}>
+    <div className={` ${disableScroll ? 'h-screen bg-gray-50 flex flex-col overflow-hidden ' : 'min-h-screen bg-gray-50'}`}>
+          
       {/* 메인 콘텐츠 영역 - 네비게이션이 있으면 그만큼 공간 제외 */}
       <div 
         className="flex-1 w-full overflow-auto"
@@ -36,5 +37,6 @@ const NavigationLayout: React.FC<NavigationLayoutProps> = ({
     </div>
   );
 };
+
 
 export default NavigationLayout;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
